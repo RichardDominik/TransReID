a37055461021f9c8a3ba2c5d5c3676bb092ccce0
Richard <riso.dominik@gmail.com>
2022-02-03 12:48:39 +0100

    wip

--- swin-visualization.sh
+++ swin-visualization.sh
@@ -1,5 +1,7 @@
-destinationLogPath='./logs/swin_visualization_trained_120_epochs.log'
+destinationLogPath='./logs/swin_vis_120_epochs.log'
 trainedPth='./logs/veri_swin_transformer_v2/swin_transformer_120.pth'
 config='configs/VeRi/swin_transformer_v2.yml'
 
-nohup python visualize.py --config_file $config MODEL.DEVICE_ID "('0')" TEST.WEIGHT $trainedPth > $destinationLogPath 2>&1 &
+#nohup python visualize.py --config_file $config MODEL.DEVICE_ID "('0')" TEST.WEIGHT $trainedPth > $destinationLogPath 2>&1 &
+
+nohup python tools/get_vis_results.py --config_file $config MODEL.DEVICE_ID "('0')" TEST.WEIGHT $trainedPth > $destinationLogPath 2>&1 &
