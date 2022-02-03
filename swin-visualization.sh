e3a3a4cd85b989453ca2abd3d3c2c9d2c50cb887
Richard <riso.dominik@gmail.com>
2022-02-03 13:02:29 +0100

    wip

--- swin-visualization.sh
+++ swin-visualization.sh
@@ -4,4 +4,4 @@ config='configs/VeRi/swin_transformer_v2.yml'
 
 #nohup python visualize.py --config_file $config MODEL.DEVICE_ID "('0')" TEST.WEIGHT $trainedPth > $destinationLogPath 2>&1 &
 
-nohup python tools/get_vis_results.py --config_file $config MODEL.DEVICE_ID "('0')" TEST.WEIGHT $trainedPth > $destinationLogPath 2>&1 &
+nohup python get_vis_results.py --config_file $config MODEL.DEVICE_ID "('0')" TEST.WEIGHT $trainedPth > $destinationLogPath 2>&1 &
